4280e3e826332a45af0aacefa8483f3645cdf259
Witizli <benaiah.finkel@heig-vd.ch>
2022-03-25 11:47:13 +0100

    ex04_graphique_statique_2022_03_25_final_V2

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -1,26 +1,12 @@
 import * as d3 from "d3";
 
 // Pour importer les données
+// Permet d'importer les données lors ce qu'elles viennt d'un fichier CSV en local
 import PIB from "../data/income_per_person_gdppercapita_ppp_inflation_adjusted.csv";
 import life from "../data/life_expectancy_years.csv";
 import population from "../data/population_total.csv";
 
-// set the dimensions and margins of the graph
-const margin = { top: 10, right: 20, bottom: 30, left: 50 },
-  width = 1000 - margin.left - margin.right,
-  height = 1000 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-const svg = d3
-  .select("#my_dataviz")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
-
-buildData(PIB, life, population);
-
+// Autre technique de récupération des donné CSV, fonctionne comme si les fichier proviennet du web
 // Promise.all([
 //   d3.csv("../data/income_per_person_gdppercapita_ppp_inflation_adjusted.csv"),
 //   d3.csv("../data/life_expectancy_years.csv"),
@@ -29,10 +15,30 @@ buildData(PIB, life, population);
 //   buildData(PIB, life, population);
 // });
 
+// Configuration des dimensions en marge et hauteur du graphique
+const margin = { top: 10, right: 20, bottom: 30, left: 50 },
+  width = 1000 - margin.left - margin.right,
+  height = 1000 - margin.top - margin.bottom;
+
+// appel l'objet svg sur le body de la page
+const svg = d3
+  .select("#mes_datas")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.left},${margin.top})`);
+
+buildData(PIB, life, population);
+
 function buildData(PIB, life, population) {
   console.log("PIB", PIB[0]["2021"]);
   console.log("life", life[0]["2021"]);
   console.log("population", population[0]["2021"]);
+
+  // Je créer un nouveau tableau a partir de mes 3 fichier CSV
+  // ce qui me permet d'obtenir un tableau qui contient les 3 éléments que je souhaite utiliser
+  // qui sont contenu dans chacun des CSV
 
   let datas = [];
   PIB.map((e) => {
@@ -52,23 +58,24 @@ function buildData(PIB, life, population) {
 }
 
 function readData(data) {
-  // Add X axis
+  // Ajout X axis
   const x = d3.scaleLinear().domain([0, 100000]).range([0, width]);
   svg
     .append("g")
     .attr("transform", `translate(0, ${height})`)
     .call(d3.axisBottom(x));
 
-  // Add Y axis
+  // Ajout Y axis
   const y = d3.scaleLinear().domain([40, 100]).range([height, 0]);
   svg.append("g").call(d3.axisLeft(y));
   // });
 
-  // Add a scale for bubble size
+  // Ajoute une grandeure aux ronds
   const z = d3.scaleLinear().domain([200000, 1310000000]).range([1, 40]);
 
   console.log("test", data);
-  // Add dots
+
+  // Ajout des points
   svg
     .append("g")
     .selectAll("dot")
@@ -77,11 +84,12 @@ function readData(data) {
     .attr("cx", (d) => x(cleanData(d.PIB)))
     .attr("cy", (d) => y(cleanData(d.esperance_vie)))
     .attr("r", (d) => z(cleanData(d.population) * 5))
-    // .attr("r", 10)
     .style("fill", "#69b3a2")
     .style("opacity", "0.7")
     .attr("stroke", "black");
 }
+
+// Permet de nétoyer les donnés pour obtenir des chiffres et non plus des charactères
 
 function cleanData(data) {
   if (isNaN(data)) {
@@ -92,6 +100,10 @@ function cleanData(data) {
       const n = data.split("M")[0];
       return Number.parseFloat(n) * 1000000;
     }
+    //   else if (data.includes("B")) {
+    //   const n = data.split("B")[0];
+    //   return Number.parseFloat(n) * 1000000000;
+    // }
   }
 
   return data;
